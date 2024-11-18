17f0dd4e4b7dd883c7823a1cf2cccee2a1c0e151
Cesar Napoleon Mejia Leiva <cesarnml@outlook.com>
2024-11-18 16:05:39 +0700

    add prettier config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap'
 
 // import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cesar-mejia-blog.vercel.app",
+  site: 'https://cesar-mejia-blog.vercel.app',
   integrations: [mdx(), sitemap()],
   // output: "server",
   // adapter: vercel(),
-});
+})
