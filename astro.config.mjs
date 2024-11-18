56699d1a980ed9514b2e95bf933ecd9a713ed454
Cesar Napoleon Mejia Leiva <cesarnml@outlook.com>
2024-11-18 11:35:25 +0700

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://cesar-mejia-blog.vercel.app",
+  integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: vercel(),
 });
